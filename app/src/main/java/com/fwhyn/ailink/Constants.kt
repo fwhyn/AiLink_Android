546178b08328ed532b7afd5878049aee7935211f
fwhyn <fwhynseventh@gmail.com>
2023-07-26 22:21:57 +0700

    AiLink

--- app/src/main/java/com/fwhyn/ailink/Constants.kt
+++ app/src/main/java/com/fwhyn/ailink/Constants.kt
@@ -1,5 +1,9 @@
 package com.fwhyn.ailink
 
+// Instruction:
+// http://doc.elinkthings.com/web/#/12?page_id=72
+// http://doc.elinkthings.com/web/#/12?page_id=50
+
 object Constants {
     const val TAG = "fwhyn_tag"
 }
